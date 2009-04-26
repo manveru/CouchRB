e5e9579eb7ab83b8b7b43e2902bb9d5e474627aa
Michael Fellinger <m.fellinger@gmail.com>
2009-04-26 16:29:11 +0900

    Add some more couchdb files for testing

--- spec/couchrb/couch_file.rb
+++ spec/couchrb/couch_file.rb
@@ -1,19 +1,31 @@
 require 'spec/helper'
 
-# Pretty sad, all we can do right now is read the headers and check whether
-# they are valid.
-
 describe CouchRB::CouchFile do
-  path = File.expand_path('../../fixture/test.couch', __FILE__)
+  fixture = File.expand_path('../../fixture', __FILE__)
+  empty = File.join(fixture, 'empty.couch')
+  tiny  = File.join(fixture, 'tiny.couch')
+  medium = File.join(fixture, 'medium.couch')
 
   it 'should parse headers of a file' do
-    db = CouchRB::CouchFile.new(path)
+#     db = CouchRB::CouchFile.new(empty)
+#     db.parse
+#
+#     db.header1.should.be.ok
+#     db.header1.md5.should == "2309e1153e6794f26f9a6504f4a2d1f9"
+#
+#     db.header2.should.be.ok
+#     db.header2.md5.should == db.header1.md5
+  end
+
+  it 'should parse more!' do
+    db = CouchRB::CouchFile.new(tiny)
+    puts
     db.parse
+  end
 
-    db.header1.should.be.ok
-    db.header1.md5.should == "2309e1153e6794f26f9a6504f4a2d1f9"
-
-    db.header2.should.be.ok
-    db.header2.md5.should == db.header1.md5
+  it 'should parse even more!' do
+#     db = CouchRB::CouchFile.new(medium)
+#     puts
+#     db.parse
   end
 end
