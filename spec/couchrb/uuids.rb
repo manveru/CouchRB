0bcf77f06ee0f583dab8e39952d81e56fcee6b07
Michael Fellinger <m.fellinger@gmail.com>
2009-05-04 14:34:29 +0900

    Fix uuid spec

--- spec/couchrb/uuids.rb
+++ spec/couchrb/uuids.rb
@@ -4,7 +4,7 @@ describe '/_uuids' do
   behaves_like :couch_client
 
   def ensure_hash_busting_headers
-    res = last_response
+    res = @db.last_response
 
     res['Cache-Control'].should =~ /no-cache/
     res['Pragma'].should =~ /no-cache/
@@ -17,18 +17,20 @@ describe '/_uuids' do
     (current_time - date_header).should < 30000
   end
 
-  it 'should return a single UUID without an explicit count' do
-    get('/_uuids').status.should == 200
-    body = json_body
+  @db = DB.new('/test_suite_db')
+
+  it 'returns a single UUID without an explicit count' do
+    @db.get('/_uuids')
+    body = @db.json_body
     body['uuids'].size.should == 1
     @first = body['uuids'][0]
 
     ensure_hash_busting_headers
   end
 
-  it 'should return a single UUID with an explicit count' do
-    get('/_uuids', :count => 1).status.should == 200
-    body = json_body
+  it 'returns a single UUID with an explicit count' do
+    @db.get('/_uuids', :count => 1)
+    body = @db.json_body
     body['uuids'].size.should == 1
     @second = body['uuids'][0]
 
@@ -36,13 +38,13 @@ describe '/_uuids' do
   end
 
   should 'have no collisions with 1,000 UUIDs' do
-    get('/_uuids', :count => 1000)
-    body = json_body
+    @db.get('/_uuids', :count => 1000)
+    body = @db.json_body
     body['uuids'].size.should == 1000
     body['uuids'].uniq.size.should == 1000
   end
 
   should 'return a 405 on POST' do
-    post('/_uuids', :count => 1000).status.should == 405
+    @db.post('/_uuids', :count => 1000).status.should == 405
   end
 end
